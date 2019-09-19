fb91687998b32fc898c80bc822d4850c46059f81
Phoenix Kahlo <phoenix@correlation-one.com>
2019-09-19 11:19:31 -0500

    shortcut scripted commit rand=9454

--- scripts/ll.sh
+++ scripts/ll.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# export COLUMNS=$( (( $COLUMNS <= 100 )) && echo "$COLUMNS" || echo "100" )
-export COLUMNS=$( if (( $COLUMNS <= 100 )); then echo "$COLUMNS"; else echo "100"; fi )
+export COLUMNS=$( [[ ! -z "$COLUMNS" ]] && (( "$COLUMNS" <= 100 ))  && echo "$COLUMNS" || echo "100" )
+#export COLUMNS=$( if (( $COLUMNS <= 100 )); then echo "$COLUMNS"; else echo "100"; fi )
 
 exa -G --across --group-directories-first -F $@
