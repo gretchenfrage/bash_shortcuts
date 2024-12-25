a4dcb128861cea0009b2b8fee2375423d442d48d
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2024-12-25 12:33:37 -0600

    shortcut scripted commit rand=24010

--- scripts/preamble.sh
+++ scripts/preamble.sh
@@ -2,6 +2,6 @@
 
 # print preamble for Rust GitHub issue messages
 
-OUTPUT="On current $(git rev-parse --abbrev-ref HEAD) (\`$(git rev-parse HEAD)\`) with Rust $(rustc --version | cut -d' ' -f2 | cut -d'.' -f1,2) (\`$(rustc --version)\`, \`$(cargo --version)\`), "
+OUTPUT="On current $(git rev-parse --abbrev-ref HEAD) (\$(git rev-parse HEAD)\) with Rust $(rustc --version | cut -d' ' -f2 | cut -d'.' -f1,2) (\`$(rustc --version)\`, \`$(cargo --version)\`), "
 echo -n "${OUTPUT}"
 echo -n "${OUTPUT}" | xclip
